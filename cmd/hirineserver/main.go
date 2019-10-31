5577a6646180b5cccb58d49bfea7c07dbd0ece66
Ren <r2life.contact@gmail.com>
2019-10-31 09:25:58 -0400

    global log setting

--- cmd/hirineserver/main.go
+++ cmd/hirineserver/main.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"hirine/app"
+	"log"
 	"os"
 	"time"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-func main() {
-	log.SetFormatter(&log.JSONFormatter{
-		FieldMap: log.FieldMap{
-			log.FieldKeyTime:  "timestamp",
-			log.FieldKeyLevel: "severity",
-			log.FieldKeyMsg:   "message",
+func init() {
+	log := logrus.New()
+	log.SetFormatter(&logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "severity",
+			logrus.FieldKeyMsg:   "message",
 		},
 		TimestampFormat: time.RFC3339Nano,
 	})
 	log.SetOutput(os.Stdout)
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(logrus.DebugLevel)
+
 	viper.SetConfigName("config.dev")
 	viper.AddConfigPath("./config")
+}
+
+func main() {
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Panicf("Fatal error config file: %s \n", err)
